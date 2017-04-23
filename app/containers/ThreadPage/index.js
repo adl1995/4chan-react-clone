99fb93b448c830edb20d5a106956cac345549c2c
Adeel Ahmad <adeelahmad14@hotmail.com>
2017-04-23 11:22:07 +0500

    Install axios for sending API calls

--- app/containers/ThreadPage/index.js
+++ app/containers/ThreadPage/index.js
@@ -4,6 +4,7 @@
  * List thread in a board
  */
 import React from 'react';
+import axios from 'axios';
 import H1 from 'components/H1';
 
 export default class ThreadPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -16,9 +17,13 @@ export default class ThreadPage extends React.Component { // eslint-disable-line
 
   render() {
   	const board = this.props.routeParams.tag;
+  	axios.get('https://a.4cdn.org/'+board+'/threads.json')
+	  .then(function (response) {
+	    console.log(response.data);
+    })
     return (
 	    <H1>
-	      Welcome to the {board} thread
+	      Welcome to the '{board}' thread
 	    </H1>
     );
   }
